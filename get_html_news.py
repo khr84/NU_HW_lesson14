981630a738ea179f86111f2c3af241c78189247d
khusnutdinov <roman_kh@ngs.ru>
2023-02-06 22:41:15 +0700

    add utf8 for file write

--- get_html_news.py
+++ get_html_news.py
@@ -17,7 +17,7 @@ while not end_loop:
 url = f'{domain}/pulse/{date_in}'
 
 response = requests.get(url)
-print(response.status_code)
+#print(response.status_code)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 link_list = soup.findAll('a', class_ = 'jsx-353754120 link _2rfL2rmI jsx-2439255140 link')
@@ -29,12 +29,12 @@ list_news = []
 i = 0
 for link_item in link_list:
     dict_new = {}
-    time.sleep(5)
+    time.sleep(3)
     responce = requests.get(f'{domain}{link_item.get("href")}')
     # print(f'{domain}{link_item.get("href")}')
     # print(responce.status_code)
     soup = BeautifulSoup(responce.text, 'html.parser')
-    link_text = soup.find('h1', class_ = 'jsx-1897426106 title').text
+    link_text = soup.find('h1', class_ = 'jsx-2784051990 _1colu_j_').text
     link_date = soup.find('div', class_ = '_1Lg_CbTX _240YeLMx').text
     dict_new['news_text'] = link_text
     dict_new['news_date'] = link_date
@@ -42,5 +42,5 @@ for link_item in link_list:
     list_news.append(dict_new)
 
 # print(list_news)
-with open('news_file.txt', 'a') as f:
+with open('news_file.txt', 'a', encoding='utf8') as f:
     f.write(str(list_news))
